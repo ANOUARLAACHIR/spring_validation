5b62d909f5ed8cca710c24c3eb19cb81ee7ec3d2
ANOUARLAACHIR <become.engineer88@gmail.com>
2024-10-25 16:33:21 +0100

    @RestControllerAdvice not @RestController

--- src/main/java/com/codewa/spring_validation/haldler/ExceptionHandler.java
+++ src/main/java/com/codewa/spring_validation/haldler/ExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.codewa.spring_validation.haldler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@RestControllerAdvice
 public class ExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleInvalidArguments
             (MethodArgumentNotValidException ex) {
